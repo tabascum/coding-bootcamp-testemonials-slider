3f9e91ab03541fe6bfaeb995a463f6a532dd7450
tabascum <marques_vasco@yahoo.es>
2023-07-12 23:51:07 +0200

    Update the main layout for mobile

--- public/js/main.js
+++ public/js/main.js
@@ -1,15 +1,34 @@
 const slidesContainer = document.querySelector(".slide-container");
-const slide = document.querySelector(".slide");
 const prevButton = document.getElementById("slide-prev");
 const nextButton = document.getElementById("slide-next");
 
-nextButton.addEventListener("click", () => {
-  console.log("Click");
-  const slideWidth = slide.clientWidth;
-  slidesContainer.scrollLeft += slideWidth;
-});
+/* let i = 0;
 
-prevButton.addEventListener("click", () => {
-  const slideWidth = slide.clientWidth;
-  slidesContainer.scrollLeft -= slideWidth;
-});
+function display(i) {
+  slidesContainer.forEach((slide) => {
+    slide.style.display = "none";
+  });
+  slidesContainer[i].style.display = "flex";
+}
+
+display(i);
+
+function nextSlide() {
+  i++;
+  if (i > slidesContainer.length - 1) {
+    i = 0;
+  }
+  display(i);
+}
+
+function prevSlide() {
+  i--;
+  if (i < 0) {
+    i = slidesContainer.length - 1;
+  }
+  display(i);
+}
+
+nextButton.addEventListener("click", nextSlide);
+prevButton.addEventListener("click", prevSlide);
+ */
